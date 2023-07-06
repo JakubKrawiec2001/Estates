2e1d7ac45fec4d018d770bd05922f63815894611
Jakub <kubakrawieckk04@gmail.com>
2023-07-06 14:35:28 +0200

    Made contact section for desktop devices

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Service from "./components/Service/Service";
 import Questions from "./components/Questions/Questions";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
 
 function App() {
 	return (
@@ -16,6 +17,7 @@ function App() {
 			<Service></Service>
 			<Questions></Questions>
 			<Projects></Projects>
+			<Contact></Contact>
 			<Footer></Footer>
 		</div>
 	);

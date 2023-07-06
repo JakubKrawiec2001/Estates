bec08499b89fa7e2701b38f52e90b73e10fcc7fe
Jakub <kubakrawieckk04@gmail.com>
2023-07-06 16:59:45 +0200

    Made contact section for mobile devices

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -16,14 +16,16 @@ const Contact = () => {
 				</h2>
 				<div className="contact-container">
 					<div className="contact-l">
-						<p className="contact-text">
-							<MdLocationOn className="contact-l-icon"></MdLocationOn>250-200
-							10th Ave N, Nashville, TN 37203, United States
-						</p>
-						<p className="contact-text">
-							<GrMail className="contact-l-icon"></GrMail> homefort@office.com
-						</p>
+						<div className="contact-text">
+							<MdLocationOn className="contact-l-icon"></MdLocationOn>
+							<p>250-200 10th Ave N, Nashville, TN 37203, United States</p>
+						</div>
+						<div className="contact-text">
+							<GrMail className="contact-l-icon"></GrMail>
+							<p>homefort@office.com</p>
+						</div>
 						<iframe
+                        title="googleMap"
 							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3221.260436687745!2d-86.78828082369259!3d36.16021660351302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886466f43ca91f2b%3A0x14b3ff0bbaadda29!2s250%2010th%20Ave%20N%2C%20Nashville%2C%20TN%2037203%2C%20Stany%20Zjednoczone!5e0!3m2!1spl!2spl!4v1688643442956!5m2!1spl!2spl"
 							allowfullscreen=""
 							loading="lazy"
